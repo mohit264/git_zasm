dd4ca5210671c1cf73ecf3ab950a963a19b63ff4
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2025-01-13 09:53:19 +0530

    Added user registration backend functionality

---
+++ userReg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void userRegistration(void);
+
+int main(void)
+{
+	printf("Inside Main function");
+	userRegistration();
+	return 0;
+}
+
+void userRegistration(void)
+{
+	printf("Inside User Registration Function");
+}
